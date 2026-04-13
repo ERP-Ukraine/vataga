bece6d0e123f0f413e7d9b3422a38b516768237a
Герасименко Вадим Миколайович <vadym.h@robots.com.ua>
2026-04-13 09:39:59 +0300

    Add Product Vataga module
    - Introduced a new module named 'Product Vataga' for enhanced product labeling.
    - Created the module's manifest file with necessary metadata and dependencies.
    - Added an XML template for custom product label reports, improving label presentation and functionality.

---
+++ extra-addons/product_vataga/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Product Vataga',
+    'version': '1.0',
+    'category': 'Inventory',
+    'author': 'ERP Ukraine LLC',
+    'website': 'https://erp.co.ua',
+    'support': 'support@erp.co.ua',
+    'license': 'LGPL-3',
+    'auto_install': False,
+    'installable': True,
+    'application': False,
+    'depends': [
+        'product',
+    ],
+    'data': [
+        'views/report_product_label_dymo.xml',
+    ],
+}
