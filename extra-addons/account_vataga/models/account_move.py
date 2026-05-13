63736f208fc7a0113bfa84086710343eb29f2990
Герасименко Вадим Миколайович <vadym.h@robots.com.ua>
2026-05-13 12:03:35 +0300

    Enhance account.move model and view with new moderation fields and improved write logic
    - Added moderation field descriptions for better tracking of approval statuses.
    - Implemented logic to reset specific boolean fields when transitioning to draft state.
    - Updated the write method to handle cash flow item accounts more effectively.
    - Modified the account_move XML view to improve visibility and management of invoice line items based on state.

--- extra-addons/account_vataga/models/account_move.py
+++ extra-addons/account_vataga/models/account_move.py
@@ -4,6 +4,12 @@ from odoo import _, fields, models
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
+    _studio_moderation_field_descriptions = {
+        'Модератор OK',
+        'Бухгалтер OK',
+        'Тарас OK',
+    }
+
     project_account_id = fields.Many2one(
         'account.analytic.account', domain="[('is_plan_project', '=', True)]"
     )
@@ -16,3 +22,43 @@ class AccountMove(models.Model):
     seller_contract_id = fields.Many2one(
         'account.analytic.account', domain="[('is_plan_seller_contract', '=', True)]"
     )
+
+    def _get_studio_boolean_fields_to_reset(self):
+        fields_to_reset = self.env['ir.model.fields'].sudo().search([
+            ('model', '=', 'account.move'),
+            ('ttype', '=', 'boolean'),
+            ('state', '=', 'manual'),
+            ('name', '=like', 'x_studio_%'),
+            ('field_description', 'in', list(self._studio_moderation_field_descriptions)),
+        ])
+        return [field.name for field in fields_to_reset if field.name in self._fields]
+
+    def write(self, vals):
+        posted_invoice_lines = self.env['account.move.line']
+        if 'cash_flow_item_account_id' in vals:
+            posted_invoice_lines = self.filtered(
+                lambda move: move.state == 'posted' and move.is_invoice(include_receipts=True)
+            ).invoice_line_ids
+
+        res = super().write(vals)
+
+        if posted_invoice_lines:
+            posted_invoice_lines._inverse_analytic_distribution()
+
+        return res
+
+    def button_draft(self):
+        posted_invoice_moves = self.filtered(
+            lambda move: move.state == 'posted' and move.is_invoice(include_receipts=True)
+        )
+
+        res = super().button_draft()
+
+        fields_to_reset = self._get_studio_boolean_fields_to_reset()
+        if posted_invoice_moves and fields_to_reset:
+            posted_invoice_moves.write({
+                field_name: False
+                for field_name in fields_to_reset
+            })
+
+        return res
