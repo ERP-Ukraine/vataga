54d7eb4c6b873d64f3cb60e01df4067a2bb44995
Герасименко Вадим Миколайович <vadym.h@robots.com.ua>
2026-05-25 06:41:06 +0300

    Move moderation reset to separate addon

--- extra-addons/account_vataga/models/account_move_line.py
+++ extra-addons/account_vataga/models/account_move_line.py
@@ -10,4 +10,17 @@ class AccountMoveLine(models.Model):
         'move_id.cash_flow_item_account_id', 'move_id.seller_contract_id'
     )
     def _compute_analytic_distribution(self):
-        return super()._compute_analytic_distribution()
+        for line in self:
+            if line.display_type == 'product' or not line.move_id.is_invoice(include_receipts=True):
+                set_analytic_accounts = [
+                    str(account.id) for account in [
+                        line.move_id.project_account_id,
+                        line.move_id.budget_account_id,
+                        line.move_id.cash_flow_item_account_id,
+                        line.move_id.seller_contract_id
+                    ] if account]
+                if set_analytic_accounts:
+                    ids_sts = ','.join(sorted(set_analytic_accounts))
+                    line.analytic_distribution = {ids_sts: 100}
+            else:
+                super(AccountMoveLine, line)._compute_analytic_distribution()
