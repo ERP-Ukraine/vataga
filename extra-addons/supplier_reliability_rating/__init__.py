02eb366868c99c27732e6aa48a5dfdc1cadc3442
Герасименко Вадим Миколайович <vadym.h@robots.com.ua>
2026-05-25 05:45:43 +0300

    Revert "Use ERP UA post init hook style"
    This reverts commit 94a86c4f2d0aa1bac1da3e83bdd299ccafddfd00.

--- extra-addons/supplier_reliability_rating/__init__.py
+++ extra-addons/supplier_reliability_rating/__init__.py
@@ -1,7 +1,10 @@
 from . import models
 
+from odoo import SUPERUSER_ID, api
 
-def post_init_hook(env):
+
+def post_init_hook(cr, registry):
+    env = api.Environment(cr, SUPERUSER_ID, {})
     trial_rating = env.ref(
         "supplier_reliability_rating.supplier_reliability_rating_trial",
         raise_if_not_found=False,
@@ -9,11 +12,9 @@ def post_init_hook(env):
     if not trial_rating:
         return
 
-    env.cr.execute(
-        """
-        UPDATE res_partner
-           SET supplier_reliability_rating_id = %s
-         WHERE supplier_reliability_rating_id IS NULL
-        """,
-        [trial_rating.id],
-    )
+    partners = env["res.partner"].with_context(active_test=False).search([])
+    partners_without_rating = partners.filtered_domain([
+        ("supplier_reliability_rating_id", "=", False),
+    ])
+    partners_without_rating.write({"supplier_reliability_rating_id": trial_rating.id})
+    partners._compute_display_name()
