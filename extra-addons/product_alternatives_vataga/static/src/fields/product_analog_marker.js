5202b541aab022a2633b75e5761ce34a2a2f9903
Герасименко Вадим Миколайович <vadym.h@robots.com.ua>
2026-05-27 13:20:58 +0300

    Simplify BOM analog dropdown

--- extra-addons/product_alternatives_vataga/static/src/fields/product_analog_marker.js
+++ extra-addons/product_alternatives_vataga/static/src/fields/product_analog_marker.js
@@ -1,35 +1,14 @@
 /** @odoo-module **/
 
-import { Component, onMounted, onPatched, onWillUnmount, useRef, useState } from "@odoo/owl";
+import { Component, onMounted, onPatched, useRef } from "@odoo/owl";
 import { registry } from "@web/core/registry";
-import { useService } from "@web/core/utils/hooks";
 import { standardFieldProps } from "@web/views/fields/standard_field_props";
-
-let activeAnalogMarkerField = null;
 
 export class ProductAnalogMarkerField extends Component {
     setup() {
-        this.orm = useService("orm");
         this.root = useRef("root");
-        this.state = useState({ open: false, analogRows: [], menuStyle: "" });
-        this.closeDropdown = this.closeDropdown.bind(this);
-        this.onDocumentClick = this.onDocumentClick.bind(this);
-        this.onWindowScroll = this.onWindowScroll.bind(this);
-        onMounted(() => {
-            this.clearCellTitle();
-            document.addEventListener("click", this.onDocumentClick);
-            window.addEventListener("scroll", this.onWindowScroll, true);
-            window.addEventListener("resize", this.closeDropdown);
-        });
+        onMounted(() => this.clearCellTitle());
         onPatched(() => this.clearCellTitle());
-        onWillUnmount(() => {
-            document.removeEventListener("click", this.onDocumentClick);
-            window.removeEventListener("scroll", this.onWindowScroll, true);
-            window.removeEventListener("resize", this.closeDropdown);
-            if (activeAnalogMarkerField === this) {
-                activeAnalogMarkerField = null;
-            }
-        });
     }
 
     get marker() {
@@ -41,7 +20,7 @@ export class ProductAnalogMarkerField extends Component {
     }
 
     get analogRows() {
-        return this.state.analogRows;
+        return this.fallbackAnalogRows;
     }
 
     get markerAnalogNames() {
@@ -60,81 +39,6 @@ export class ProductAnalogMarkerField extends Component {
                 analog: analogParts.join("\t") || component || "",
             };
         });
-    }
-
-    get recordModel() {
-        return (
-            this.props.record.resModel ||
-            this.props.record.model?.root?.resModel ||
-            this.props.record.model?.config?.resModel
-        );
-    }
-
-    get recordId() {
-        return this.props.record.resId || this.props.record.data.id || this.props.record.evalContext?.id;
-    }
-
-    async loadAnalogNames() {
-        if (this.fallbackAnalogRows.length) {
-            this.state.analogRows = this.fallbackAnalogRows;
-            return;
-        }
-        if (!this.recordModel || !this.recordId) {
-            this.state.analogRows = [];
-            return;
-        }
-        try {
-            const lines = await this.orm.call(
-                this.recordModel,
-                "get_analog_product_names",
-                [[this.recordId]]
-            );
-            this.state.analogRows = lines.map((line) => {
-                const [component, ...analogParts] = line.split("\t");
-                return {
-                    component: component || "",
-                    analog: analogParts.join("\t") || component || "",
-                };
-            });
-        } catch {
-            this.state.analogRows = [];
-        }
-    }
-
-    async toggleDropdown(ev) {
-        ev.stopPropagation();
-        if (this.state.open) {
-            this.closeDropdown();
-            return;
-        }
-        if (activeAnalogMarkerField && activeAnalogMarkerField !== this) {
-            activeAnalogMarkerField.closeDropdown();
-        }
-        activeAnalogMarkerField = this;
-        this.state.menuStyle = "";
-        this.state.open = true;
-        if (!this.state.analogRows.length) {
-            await this.loadAnalogNames();
-        }
-    }
-
-    closeDropdown() {
-        this.state.open = false;
-        if (activeAnalogMarkerField === this) {
-            activeAnalogMarkerField = null;
-        }
-    }
-
-    onDocumentClick(ev) {
-        if (this.state.open && !this.root.el?.contains(ev.target)) {
-            this.closeDropdown();
-        }
-    }
-
-    onWindowScroll(ev) {
-        if (this.state.open && !this.root.el?.contains(ev.target)) {
-            this.closeDropdown();
-        }
     }
 
     clearCellTitle() {
