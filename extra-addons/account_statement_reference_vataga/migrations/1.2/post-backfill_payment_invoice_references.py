8073f21071c8b41f37dd2e20cc103e045697a077
Герасименко Вадим Миколайович <vadym.h@robots.com.ua>
2026-05-29 12:25:32 +0300

    Remove account statement reference addon

--- extra-addons/account_statement_reference_vataga/migrations/1.2/post-backfill_payment_invoice_references.py
+++
@@ -1,8 +0,0 @@
-from odoo import SUPERUSER_ID, api
-
-
-def migrate(cr, version):
-    env = api.Environment(cr, SUPERUSER_ID, {})
-    env['account.bank.statement.line']._backfill_payment_invoice_references(
-        [('is_reconciled', '=', True)]
-    )
