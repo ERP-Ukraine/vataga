1f226141c671267a3d100fd93ff35d7e53af1849
Герасименко Вадим Миколайович <vadym.h@robots.com.ua>
2026-07-02 16:05:53 +0300

    Restore moderation reset on draft

--- extra-addons/account_vataga/models/account_move.py
+++ extra-addons/account_vataga/models/account_move.py
@@ -1,4 +1,9 @@
+import logging
+
 from odoo import _, fields, models
+
+
+_logger = logging.getLogger(__name__)
 
 
 class AccountMove(models.Model):
@@ -16,3 +21,33 @@ class AccountMove(models.Model):
     seller_contract_id = fields.Many2one(
         'account.analytic.account', domain="[('is_plan_seller_contract', '=', True)]"
     )
+
+    def _get_studio_boolean_fields_to_reset(self):
+        fields_to_reset = self.env['ir.model.fields'].sudo().search([
+            ('model', '=', 'account.move'),
+            ('ttype', '=', 'boolean'),
+            ('state', '=', 'manual'),
+            ('name', '=like', 'x_studio_%'),
+        ])
+        return [
+            field.name
+            for field in fields_to_reset
+            if field.name in self._fields
+        ]
+
+    def button_draft(self):
+        posted_invoice_moves = self.filtered(
+            lambda move: move.state == 'posted' and move.move_type == 'in_invoice'
+        )
+
+        res = super().button_draft()
+
+        fields_to_reset = self._get_studio_boolean_fields_to_reset()
+        if posted_invoice_moves and fields_to_reset:
+            _logger.info("Resetting Studio boolean fields on posted vendor bills: %s", fields_to_reset)
+            posted_invoice_moves.write({
+                field_name: False
+                for field_name in fields_to_reset
+            })
+
+        return res
