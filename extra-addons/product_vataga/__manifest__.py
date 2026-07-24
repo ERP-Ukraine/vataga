862fe95cbf1bb5ba532d7f9fa8bb1933574d4689
Герасименко Вадим Миколайович <vadym.h@robots.com.ua>
2026-07-24 16:41:27 +0300

    Search products by packaging barcode

--- extra-addons/product_vataga/__manifest__.py
+++ extra-addons/product_vataga/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Product and transaction labels',
-    'version': '1.8',
+    'version': '1.9',
     'category': 'Inventory',
     'author': 'Vataga',
     'license': 'LGPL-3',
@@ -13,6 +13,7 @@
         # 'stock',
     ],
     'data': [
+        'views/product_search_views.xml',
         'views/report_product_label_dymo.xml',
         # Transfer/package labels are disabled; keep only the product label override active.
         # 'views/stock_picking_views.xml',
