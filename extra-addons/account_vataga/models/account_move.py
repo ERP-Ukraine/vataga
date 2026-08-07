5485d09976b92e988ca667c2933018ceb6ed6a26
Герасименко Вадим Миколайович <vadym.h@robots.com.ua>
2026-08-07 14:45:11 +0300

    Restore posted invoice analytic editing

--- extra-addons/account_vataga/models/account_move.py
+++ extra-addons/account_vataga/models/account_move.py
@@ -4,6 +4,13 @@ from odoo import _, fields, models
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
+    ANALYTIC_HEADER_FIELDS = {
+        'project_account_id',
+        'budget_account_id',
+        'cash_flow_item_account_id',
+        'seller_contract_id',
+    }
+
     project_account_id = fields.Many2one(
         'account.analytic.account', domain="[('is_plan_project', '=', True)]"
     )
@@ -16,3 +23,18 @@ class AccountMove(models.Model):
     seller_contract_id = fields.Many2one(
         'account.analytic.account', domain="[('is_plan_seller_contract', '=', True)]"
     )
+
+    def write(self, vals):
+        posted_invoice_lines = self.env['account.move.line']
+        if set(vals) & self.ANALYTIC_HEADER_FIELDS:
+            posted_invoice_lines = self.filtered(
+                lambda move: move.state == 'posted'
+                and move.is_invoice(include_receipts=True)
+            ).invoice_line_ids
+
+        res = super().write(vals)
+
+        if posted_invoice_lines:
+            posted_invoice_lines._inverse_analytic_distribution()
+
+        return res
