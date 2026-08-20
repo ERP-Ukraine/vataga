b812a2f18f3e0ad175c2f9e4f76ac2ec2b3acb05
VadimGRSM <vadimgrsm8923@gmail.com>
2026-08-20 10:54:15 +0300

    show quality check arrival details

--- extra-addons/quality_vataga/__manifest__.py
+++ extra-addons/quality_vataga/__manifest__.py
@@ -1,7 +1,7 @@
 {
     'name': 'Параметри якості обладнання Vataga',
     'summary': 'Параметри обладнання та налаштування контролю якості',
-    'version': '17.0.2.17',
+    'version': '17.0.2.18',
     'category': 'Manufacturing/Quality',
     'author': 'Vataga',
     'license': 'LGPL-3',
--- extra-addons/quality_vataga/tests/__init__.py
+++ extra-addons/quality_vataga/tests/__init__.py
@@ -1,3 +1,4 @@
 from . import test_quality_control_parameter_line
 from . import test_quality_check_measurement_matrix
 from . import test_quality_check_product_quantity
+from . import test_quality_check_arrival_details
