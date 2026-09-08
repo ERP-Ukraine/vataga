a8a59c6d471448d98bff779a07f84a18cebea695
VadimGRSM <vadimgrsm8923@gmail.com>
2026-09-08 17:13:32 +0300

    Add blank actual quantity column to internal picking reports

--- extra-addons/product_vataga/__manifest__.py
+++ extra-addons/product_vataga/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Product and transaction labels',
-    'version': '1.12',
+    'version': '1.13',
     'category': 'Inventory',
     'author': 'Vataga',
     'license': 'LGPL-3',
@@ -18,6 +18,7 @@
         'views/report_product_label_dymo.xml',
         'views/stock_picking_views.xml',
         'report/report_deliveryslip.xml',
+        'report/report_picking.xml',
         # Transfer/package labels are disabled; keep only the product label override active.
         # 'report/stock_picking_transfer_label.xml',
     ],
